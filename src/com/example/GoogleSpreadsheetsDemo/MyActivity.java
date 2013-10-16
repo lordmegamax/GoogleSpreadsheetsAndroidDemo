7cde870326705f330eff36ba6105c4dbd382f299
Max Melnychuk <lordmegamax@gmail.com>
2013-10-16 16:47:45 +0300

    Initial commit. Empty project.

---
+++ src/com/example/GoogleSpreadsheetsDemo/MyActivity.java
@@ -0,0 +1,15 @@
+package com.example.GoogleSpreadsheetsDemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+}
+
